a568911b389694c1039649f7c88f74c0766db44b
xaxaxa <xaxaxa@xa.us.to>
2018-02-19 10:48:30 +0000

    tester: add programming feature

--- tester/main.C
+++ tester/main.C
@@ -8,11 +8,16 @@
 #include <vector>
 #include <complex>
 #include <string>
+#include <functional>
+#include <iostream>
+#include <fstream>
 
 #include <xavna/xavna.h>
 #include <xavna/calibration.H>
 
 #include "ui.H"
+#include "platform.H"
+#include "svfplayer/libsvfplayer.H"
 
 using namespace std;
 
@@ -21,7 +26,7 @@ constexpr int nPoints=50;		// how many frequency points
 constexpr int startFreq=137500;	// start frequency in kHz
 constexpr int freqStep=50000;		// frequency step in kHz
 constexpr int nValues=50;
-bool detailed = false;
+bool detailed = true;
 void* vna_dev=NULL;
 
 
@@ -104,6 +109,12 @@ void showResult() {
 
 #define BUG() fail(ERR_BUG, "line " + to_string(__LINE__) + ": " + string(strerror(errno)))
 
+// hack to make it easy to start a thread with a std::function
+void* callStdFunction(void* param) {
+	function<void()>* func = (function<void()>*) param;
+	(*func)();
+}
+
 constexpr int queueSize=nPoints*3;
 volatile xavna_raw2_values circularQueue[queueSize];
 volatile int queueWriteIndex=0;
@@ -156,27 +167,6 @@ vector<xavna_raw2_values> doScan(int discardPoints=10) {
 	}
 	return res;
 }
-/*
-vector<xavna_raw2_values> doScan() {
-	vector<xavna_raw2_values> res;
-	res.resize(nPoints);
-	xavna_raw2_values values;
-	
-	
-	for(int i=0;i<nPoints;i++) {
-		int freq = startFreq+freqStep*i;
-		int freq2;
-		if((freq2=xavna_set_frequency(vna_dev,freq)) != freq) {
-			fail(ERR_CONTROL, "device could not set frequency to " + to_string(freq)
-				+ ", device reported " + to_string(freq2) + "; " + string(strerror(errno)));
-		}
-		if(xavna_read_values_raw2(vna_dev,&values,nValues)<=0) {
-			fail(ERR_COMMS, "xavna_read_values returned no values: " + string(strerror(errno)));
-		}
-		res[i] = values;
-	}
-	return res;
-}*/
 
 double dB(double pwr) {
 	return log10(pwr)*10;
@@ -198,7 +188,86 @@ double sensitivity(cal_t coeffs) {
 	return dB(worst)*2;
 }
 
+string svfToWaveform(string svf) {
+	svfParser parser;
+	svfPlayer player;
+	parser.reset();
+	player.reset();
+	
+	std::stringstream ss(svf);
+    std::string item;
+    int cmds=0;
+    while (std::getline(ss, item, '\n')) {
+        parser.processLine(item.c_str(),item.length());
+		svfCommand cmd;
+		while(parser.nextCommand(cmd)) {
+			player.processCommand(cmd);
+			cmds++;
+		}
+    }
+	return player.outBuffer;
+}
 
+int playSvf(string svf) {
+	string waveform = svfToWaveform(svf);
+	return platform_writeJtag((uint8_t*)waveform.data(),waveform.length());
+}
+
+bool detectJtag() {
+	int res = playSvf(
+		"ENDIR IDLE; ENDDR IDLE; STATE RESET; STATE IDLE;\
+		SIR 6 TDI (09) SMASK (3f);\
+		SDR 32 TDI (00000000) SMASK (ffffffff) TDO (f4001093) MASK (0fffffff);");
+	if(res == 0) {
+		fprintf(stderr, "FPGA detected on JTAG\n");
+	}
+	return res==0;
+}
+void doProgram() {
+	ifstream t("a.svf");
+	string str((istreambuf_iterator<char>(t)),
+					 istreambuf_iterator<char>());
+	string waveform = svfToWaveform(str);
+	fprintf(stderr, "loaded svf; %d bytes; total jtag cycles: %d\n", str.length(), waveform.length());
+	
+	if(waveform.length() == 0) {
+		ui_show_details("ERROR", "No a.svf file found", 0xff0000);
+		return;
+	}
+	
+	int blockSize=500000;
+	for(int i=0;i<waveform.length();i+=blockSize) {
+		int len=waveform.length()-i;
+		if(len>blockSize) len=blockSize;
+		int ret = platform_writeJtag((uint8_t*)waveform.data()+i,len);
+		if(ret != 0) {
+			string msg = ssprintf(64, "jtag error: %d", ret);
+			ui_show_details("ERROR", msg, 0xff0000);
+			return;
+		}
+		int percent = (i+len)*100/waveform.length();
+		ui_show_banner(ssprintf(64, "%d%%", percent), 0xbb5500);
+	}
+	ui_show_details("Done", "Please remove JTAG cable", 0x00ff00, 0x0000ff);
+}
+void tryProgram() {
+	for(int i=0; i<5; i++) {
+		if(!detectJtag()) return;
+		usleep(500000);
+	}
+	ui_show_banner("Programming", 0xbb5500);
+	doProgram();
+	
+	
+	// wait for jtag disconnect; proceed when device has been undetectable 5 times
+	int cnt=0;
+	while(true) {
+		if(detectJtag()) cnt=0;
+		else cnt++;
+		if(cnt>5) break;
+		usleep(300000);
+	}
+}
 
 int main(int argc, char** argv) {
 	if(argc<2) {
@@ -206,6 +275,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	wiringPiSetup();
+	platform_init();
 	pinMode(rfSwitchPins[0], OUTPUT);
 	pinMode(rfSwitchPins[1], OUTPUT);
 	pinMode(rfSwitchPins[2], OUTPUT);
@@ -214,11 +284,14 @@ int main(int argc, char** argv) {
 	
 	vna_dev = xavna_open(argv[1]);
 	if(vna_dev == NULL) {
-		ui_show_banner("No device");
 		while(true) {
+			usleep(500000);
+			
+			tryProgram();
+			
 			vna_dev = xavna_open(argv[1]);
 			if(vna_dev != NULL) break;
-			usleep(300000);
+			ui_show_banner("No device");
 		}
 	}
 	
@@ -256,6 +329,8 @@ int main(int argc, char** argv) {
 	int worst_sensitivity_freq = 0;
 	vector<double> s11floors(nPoints);
 	vector<int> worst_s11floors(nPoints);
+	vector<double> s21dr(nPoints);
+	vector<int> worst_s21dr(nPoints);
 	for(int i=0;i<nPoints;i++) {
 		int freq = startFreq + i*freqStep;
 		auto S = values_short[i].refl, O = values_open[i].refl, L = values_load[i].refl;
@@ -265,6 +340,8 @@ int main(int argc, char** argv) {
 		double sens = sensitivity(SOL_compute_coefficients(
 			values_short[i].rawRefl,values_open[i].rawRefl,values_load[i].rawRefl));
 		double s11floor = dB(norm(SOL_compute_reflection(cal_coeffs[i], values_load2[i].refl)));
+		s21dr[i] = dB(norm(values_thru[i].thru)) - dB(norm(values_load[i].thru));
+		worst_s21dr[i] = i;
 		
 		if(dir < worst_directivity) {
 			worst_directivity = dir;
@@ -274,6 +351,7 @@ int main(int argc, char** argv) {
 			worst_sensitivity = sens;
 			worst_sensitivity_freq = i;
 		}
+		
 		printf("%.2fMHz: directivity = %.1fdB, ", double(freq)/1000., dir);
 		printf("sensitivity = %.1fdB, ", sens);
 		printf("S11 = %.1fdB\n", s11floor);
@@ -281,11 +359,15 @@ int main(int argc, char** argv) {
 		worst_s11floors[i] = i;
 	}
 	sort(worst_s11floors.begin(), worst_s11floors.end(), [&](int a, int b) {return s11floors[a] > s11floors[b];});
+	sort(worst_s21dr.begin(), worst_s21dr.end(), [&](int a, int b) {return s21dr[a] > s21dr[b];});
+	
+	int s11floor0 = worst_s11floors[0];
+	int s11floor2 = worst_s11floors[2];
+	int s21dr2 = worst_s21dr[2];
 	
 	appendInfo(ssprintf(255, "worst directivity: \n    %.1fdB @ %.2fMHz", worst_directivity, freqAt(worst_directivity_freq)));
 	appendInfo(ssprintf(255, "worst sensitivity: \n    %.1fdB @ %.2fMHz", worst_sensitivity, freqAt(worst_sensitivity_freq)));
-	int s11floor0 = worst_s11floors[0];
-	int s11floor2 = worst_s11floors[2];
+	appendInfo(ssprintf(255, "3rd worst dynamic range: \n    %.1fdB @ %.2fMHz", s21dr[s21dr2], freqAt(s21dr2)));
 	appendInfo(ssprintf(255, "highest S11: \n    %.1fdB @ %.2fMHz", s11floors[s11floor0], freqAt(s11floor0)));
 	appendInfo(ssprintf(255, "3rd highest S11: \n    %.1fdB @ %.2fMHz", s11floors[s11floor2], freqAt(s11floor2)));
 	
@@ -299,6 +381,10 @@ int main(int argc, char** argv) {
 	}
 	if(worst_sensitivity > 30 || isnan(worst_sensitivity)) {
 		string msg = ssprintf(255, "S11 sensitivity too high: \n    %.1fdB @ %.2fMHz", worst_sensitivity, freqAt(worst_sensitivity_freq));
+		appendFail(ERR_SPEC, msg);
+	}
+	if(s21dr[s21dr2] < 30 || isnan(s21dr[s21dr2])) {
+		string msg = ssprintf(255, "S21 dynamic range too low: \n    %.1fdB @ %.2fMHz", s21dr[s21dr2], freqAt(s21dr2));
 		appendFail(ERR_SPEC, msg);
 	}
 	showResult();
