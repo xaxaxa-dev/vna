ea6a87b048831ef69825dd043aac754d58be7334
xaxaxa <xaxaxa@xa.us.to>
2018-04-05 09:51:14 +0100

    vhdl: usb: increase tx buffer size and decrease rx buffer size
    * rx is very low data rate and for configuration updates, while tx
    transfers far more data

--- vhdl/ulpi_serial.vhd
+++ vhdl/ulpi_serial.vhd
@@ -31,8 +31,8 @@ entity ulpi_serial is
 end ulpi_serial;
 
 architecture a of ulpi_serial is
-	constant RXBUFSIZE_BITS : integer := 12;
-	constant TXBUFSIZE_BITS : integer := 12;
+	constant RXBUFSIZE_BITS : integer := 10;
+	constant TXBUFSIZE_BITS : integer := 13;
 	signal ulpi_data_in, ulpi_data_out: std_logic_vector(7 downto 0);
 	signal PHY_DATABUS16_8 : std_logic;
 	signal PHY_RESET :       std_logic;
@@ -105,7 +105,7 @@ begin
 	highspeed <= usb_highspeed;
 	suspend <= usb_suspend;
 	online <= usb_online;
-	txroom <= "00"&unsigned(usb_txroom);
+	txroom <= "0"&unsigned(usb_txroom);
 	
 	clkcnt <= clkcnt + 1 when rising_edge(ulpi_clk60);
 	-- Show USB status on LED
