c1549b59df63650ccf5279f84e32f2a126aea416
xaxaxa <xaxaxa@xa.us.to>
2018-04-19 07:55:30 +0100

    deploy_linux: use nproc when building

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -16,16 +16,16 @@ QMAKE="$QT/bin/qmake"
 
 autoreconf --install
 ./configure
-make -j8
+make -j$(nproc)
 
 pushd libxavna/xavna_mock_ui
 $QMAKE
-make -j8
+make -j$(nproc)
 popd
 
 pushd vna_qt
 $QMAKE
-make -j8
+make -j$(nproc)
 popd
 
 export LD_LIBRARY_PATH="$(pwd)/libxavna/.libs:$(pwd)/libxavna/xavna_mock_ui:$QT/lib"
